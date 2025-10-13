a85c2fb477ff993f6860b95ecd49a5a36bd5c39d
Jean Edouard Roy Caro <roycaroje@gmail.com>
2025-10-13 12:52:17 +0200

    Small fix in IF CONDITION in migration

--- src/main/resources/db/migration/V20251013120549__add_indexes_to_improve_queries_performance.sql
+++ src/main/resources/db/migration/V20251013120549__add_indexes_to_improve_queries_performance.sql
@@ -1,3 +1,3 @@
-CREATE IF NOT EXISTS UNIQUE INDEX idx_client_public_id ON client(public_id);
-CREATE IF NOT EXISTS INDEX idx_contract_client_id ON contract(client_id);
-CREATE IF NOT EXISTS INDEX idx_contract_deleted_end_date ON contract(deleted, end_date);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_client_public_id ON client(public_id);
+CREATE INDEX IF NOT EXISTS idx_contract_client_id ON contract(client_id);
+CREATE INDEX IF NOT EXISTS idx_contract_deleted_end_date ON contract(deleted, end_date);
